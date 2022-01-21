d6dd606a084ef40cf1ce59d429668a42b2ed25be
Jamie Whitney <jamie@Jamies-MacBook-Pro.local>
2022-01-21 16:37:53 +0000

    initial commit

---
+++ client.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	pb "github.com/jamiewhitney/grpc-go-vault/hello"
+	"log"
+	"time"
+
+	vault "github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/sdk/helper/certutil"
+	"google.golang.org/grpc/credentials"
+
+	"fmt"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	//vault
+
+	vaultClient, err := vault.NewClient(&vault.Config{
+		Address: "http://localhost:8200",
+	})
+	if err != nil {
+		fmt.Printf("failed to create vault client: %v", err)
+	}
+
+	vaultClient.SetToken("root")
+
+	secret, err := vaultClient.Logical().Write("grpc/issue/hello-service", map[string]interface{}{
+		"common_name": "grpc.techops.blog",
+		"alt_names":   "localhost",
+	})
+	if err != nil {
+		fmt.Printf("failed to create certificate: %v", err)
+	}
+
+	// tls credentials
+
+	parsedCertBundle, err := certutil.ParsePKIMap(secret.Data)
+	if err != nil {
+		fmt.Errorf("Error parsing secret: %s", err)
+	}
+
+	tlsConfig, err := parsedCertBundle.GetTLSConfig(certutil.TLSClient)
+	if err != nil {
+		fmt.Errorf("Could not get TLS config: %s", err)
+	}
+
+	tlsCredentials := credentials.NewTLS(tlsConfig)
+
+	//grpc
+
+	x := "Jamie"
+
+	conn, err := grpc.Dial(":3000", grpc.WithTransportCredentials(tlsCredentials))
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewHelloServiceClient(conn)
+
+	for {
+		response, err := client.SayHello(context.Background(), &pb.HelloRequest{Name: x})
+		if err != nil {
+			log.Fatalf("Error when calling SayHello: %s", err)
+		}
+		log.Printf("Response from Server: %s", response.GetName())
+		time.Sleep(2 * time.Second)
+	}
+}
